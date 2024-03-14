ade36c5a855711119c0078a285e813c59f0b5139
beik <beik@rpi.edu>
2024-03-14 13:08:57 -0400

    debug

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from sys import argv
+from Random import rand_48
+
+
 if len(argv) != 8:
-    print(f"Usage: {argv[0]} [num procs] [seed] [lambda] [upper bound] [context switch time] [alpha] [time slice]")
+    print("Usage: python main.py <num> <seed> <lambda> <bound> <ctxs_time> <alpha> <time_slice>")
     exit(1)
 
 num = int(argv[1])
@@ -9,3 +13,4 @@ bound = int(argv[4])
 ctxs_time = int(argv[5])
 alpha = float(argv[6])
 time_slice = int(argv[7])
+
