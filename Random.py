ab58c629d6ae13174d5db32432402f5f9fe09b74
beik <beik@rpi.edu>
2024-03-14 13:11:26 -0400

    debug

--- Random.py
+++ Random.py
@@ -12,9 +12,6 @@ class rand_48:
     def drand48(self):
         const = (self.a * self.seed + self.c) % self.m
         return const / self.m
-    def drand48(self):
-        const = (self.a * self.seed + self.c) % self.m
-        return const / self.m
 
     def srand48(self, seed):
         self.seed = (seed << 16) + 0x330e
@@ -23,14 +20,5 @@ class rand_48:
         out = -m.log(self.drand48()) / self.mean_exp
         if out > self.upper_bound:
             return self.exp_rand48()
-        else:
-            return out
-    def srand48(self, seed):
-        self.seed = (seed << 16) + 0x330e
-    
-    def exp_rand48(self):
-        out = -m.log(self.drand48()) / self.mean_exp
-        if out > self.upper_bound:
-            return exp_rand48()
         else:
             return out
